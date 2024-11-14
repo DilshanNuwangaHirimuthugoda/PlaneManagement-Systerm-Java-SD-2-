27476a8570868e136c989aff058d819dcc82ed22
DilshanNuwangaHirimuthugoda <dilshannuwangahirimuthugoda@gmail.com>
2024-11-14 20:30:00 +0530

    Create Ticket.java file and add getters and setters.

---
+++ Ticket.java
@@ -0,0 +1,52 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//attributes
+public class Ticket {
+
+
+    private String row;
+    private int seat;
+    private double price;
+    private Person person;
+//Constructor
+    public Ticket(String row, int price, int seat, Person person) {
+        this.row = row;
+        this.seat = seat;
+        this.price = price;
+        this.person = person;
+    }
+    
+    //Get row as String
+    public String getRow() {
+        return row;
+    }
+    //Set row as String
+    public void setRow(String row) {
+        this.row = row;
+    }
+    //Get seat as String
+    public int getSeat() {
+        return seat;
+    }
+    //set seat as String
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+    //Get price as String
+    public double getPrice() {
+        return price;
+    }
+    //Set price as String
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
